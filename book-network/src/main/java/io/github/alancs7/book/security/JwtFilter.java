6ae69b5f43d118b16859032dfe305e82ef2727ce
Alan Silva <alanclash2541@gmail.com>
2024-06-27 22:59:06 -0300

    feat: Finalize the JwtFilter class

--- book-network/src/main/java/io/github/alancs7/book/security/JwtFilter.java
+++ book-network/src/main/java/io/github/alancs7/book/security/JwtFilter.java
@@ -7,6 +7,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +22,7 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -26,14 +32,32 @@ public class JwtFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-        if (request.getServletPath().contains("/api/v1/auth") || authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (isAuthHeaderValid(request, authHeader)) {
+            final String jwt = authHeader.substring(7);
+            final String username = jwtService.extractUsername(jwt);
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                var userDetails = userDetailsService.loadUserByUsername(username);
+                if (jwtService.isTokenValid(jwt, userDetails)) {
+                    authenticateUser(request, userDetails);
+                }
+            }
+
             filterChain.doFilter(request, response);
-            return;
         }
+    }
 
-        final String jwt = authHeader.substring(7);
-        final String username = jwtService.extractUsername(jwt);
+    private boolean isAuthHeaderValid(HttpServletRequest request, String authHeader) {
+        return request.getServletPath().contains("/api/v1/auth") || authHeader == null || !authHeader.startsWith("Bearer ");
+    }
 
-
+    private void authenticateUser(HttpServletRequest request, UserDetails userDetails) {
+        var authToken = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities()
+        );
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
     }
 }
