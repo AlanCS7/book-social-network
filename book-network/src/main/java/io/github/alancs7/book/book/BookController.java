640c70566269c7353e836adc6288d57327a4afaf
Alan Silva <alanclash2541@gmail.com>
2024-07-03 20:59:19 -0300

    feat: Implement the find all displayable books

--- book-network/src/main/java/io/github/alancs7/book/book/BookController.java
+++ book-network/src/main/java/io/github/alancs7/book/book/BookController.java
@@ -1,5 +1,6 @@
 package io.github.alancs7.book.book;
 
+import io.github.alancs7.book.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,4 +26,13 @@ public class BookController {
     public ResponseEntity<BookResponse> findBookById(@PathVariable("book-id") Long bookId) {
         return ResponseEntity.ok(service.findById(bookId));
     }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser) {
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
+    }
+
 }
--- book-network/src/main/java/io/github/alancs7/book/book/BookService.java
+++ book-network/src/main/java/io/github/alancs7/book/book/BookService.java
@@ -1,10 +1,17 @@
 package io.github.alancs7.book.book;
 
+import io.github.alancs7.book.common.PageResponse;
 import io.github.alancs7.book.exception.BookNotFoundException;
 import io.github.alancs7.book.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +31,24 @@ public class BookService {
         return bookRepository.findById(bookId)
                 .map(bookMapper::toBookResponse)
                 .orElseThrow(() -> new BookNotFoundException("No book found with the ID:: " + bookId));
+    }
+
+    public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
+        User user = (User) connectedUser.getPrincipal();
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
+        Page<Book> books = bookRepository.findAllDisplayableBooks(pageable, user.getId());
+        List<BookResponse> bookResponses = books.stream()
+                .map(bookMapper::toBookResponse)
+                .toList();
+
+        return new PageResponse<>(
+                bookResponses,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
     }
 }
