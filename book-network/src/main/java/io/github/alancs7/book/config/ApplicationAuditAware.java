4341c4e5af07d785405bb69e973aea53469b7180
Alan Silva <alanclash2541@gmail.com>
2024-07-02 21:39:36 -0300

    feat: Add the application auditor aware:: Auditing

---
+++ book-network/src/main/java/io/github/alancs7/book/config/ApplicationAuditAware.java
@@ -0,0 +1,22 @@
+package io.github.alancs7.book.config;
+
+import io.github.alancs7.book.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
+    }
+}
