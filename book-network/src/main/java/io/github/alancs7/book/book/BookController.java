c93c92e9d32adb32d4c8102d75382b2903da23d5
Alan Silva <alanclash2541@gmail.com>
2024-07-05 20:56:50 -0300

    feat: Implement borrow book feature

--- book-network/src/main/java/io/github/alancs7/book/book/BookController.java
+++ book-network/src/main/java/io/github/alancs7/book/book/BookController.java
@@ -78,4 +78,12 @@ public class BookController {
     ) {
         return ResponseEntity.ok(service.updateArchivedStatus(bookId, connectedUser));
     }
+
+    @PostMapping("/borrow/{book-id}")
+    public ResponseEntity<Long> borrowBook(
+            @PathVariable("book-id") Long bookId,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.borrowBook(bookId, connectedUser));
+    }
 }
