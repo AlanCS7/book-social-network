595cce8d83dbff23f29c12bf32835d31b0b95bdf
Alan Silva <alanclash2541@gmail.com>
2024-07-08 22:39:43 -0300

    fix: Include OpenApi documentation

---
+++ book-network/src/main/java/io/github/alancs7/book/config/OpenApiConfig.java
@@ -0,0 +1,54 @@
+package io.github.alancs7.book.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Book Social Network",
+                description = "OpenAPI documentation for Book Network",
+                termsOfService = "Terms of service",
+                contact = @Contact(
+                        name = "Alan Silva",
+                        email = "alancss.contact@gmail.com",
+                        url = "https://github.com/AlanCS7"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                ),
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080/api/v1"
+                ),
+                @Server(
+                        description = "Prod ENV",
+                        url = "https://someurl.com/book-network"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Authorization header using the Bearer scheme",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
